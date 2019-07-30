c9d194b2a0d8ab5ed28aec375644a5adfa687e84
seraf-z <oleg.lebedev.v@gmail.com>
2019-07-31 01:27:00 +0300

    skeleton

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    devtool: 'source-map',
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                "style-loader",
+                "css-loader",
+                "sass-loader"
+            ]
+        }]
+    },
+
+};
