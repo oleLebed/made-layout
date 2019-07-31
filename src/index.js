fecf8e996d362ecb3943d4367689545a79d95ad5
seraf-z <oleg.lebedev.v@gmail.com>
2019-07-31 15:20:14 +0300

    auto size input

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+import './normalize.scss';
 import './main.scss';
 
-console.log('HELLO WORD');
+const collection = document.getElementsByClassName('section-1__form-input');
+const hide = document.getElementById('hide');
+const hideSmall = document.getElementById('hide-small');
+
+Array.from(collection).forEach(input => input.addEventListener('input', () => {
+    const hideInput = input.clientHeight > 40 ? hide : hideSmall;
+
+    hideInput.textContent = input.value;
+    input.style.width = hideInput.offsetWidth + "px";
+}));
+
