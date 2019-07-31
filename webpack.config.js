6cdb2cb78da2d6d977def18ee6e7e33d6c692dc2
seraf-z <oleg.lebedev.v@gmail.com>
2019-07-31 11:26:58 +0300

    file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,24 @@
 module.exports = {
     devtool: 'source-map',
     module: {
-        rules: [{
-            test: /\.scss$/,
-            use: [
-                "style-loader",
-                "css-loader",
-                "sass-loader"
-            ]
-        }]
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            },
+            {
+                test: /\.(eot|ttf|woff|woff2)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+        ]
     },
 
 };
