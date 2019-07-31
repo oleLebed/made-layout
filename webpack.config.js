59060d675e8ecfb050a3c960c23b33fd6805f5c6
oleLebed <53494585+oleLebed@users.noreply.github.com>
2019-07-31 15:21:48 +0300

    Section 1 (#1)
    * file-loader
    
    * fonts
    
    * image-webpack-loader
    
    * section-1 skeleton
    
    * normalize css
    
    * auto size input

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,36 @@
 module.exports = {
     devtool: 'source-map',
     module: {
-        rules: [{
-            test: /\.scss$/,
-            use: [
-                "style-loader",
-                "css-loader",
-                "sass-loader"
-            ]
-        }]
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            disable: true,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(eot|ttf|woff|woff2)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+        ]
     },
 
 };
