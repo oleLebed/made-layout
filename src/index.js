44062f1f3e646ab217899de5fb9dbfe71e212aa5
oleLebed <53494585+oleLebed@users.noreply.github.com>
2019-08-12 19:25:56 +0300

    Issues 19 (#40)
    * div wrapper display none
    
    * error form with values
    
    * add message

--- src/index.js
+++ src/index.js
@@ -3,14 +3,27 @@ import './main.scss';
 import './button.scss';
 import './application.scss';
 
-const collection = document.getElementsByClassName('input-resize-js');
+const inputArr = Array.from(document.getElementsByClassName('input-resize-js'));
 const hide = document.getElementById('hide');
 const hideSmall = document.getElementById('hide-small');
 
-Array.from(collection).forEach(input => input.addEventListener('input', () => {
+function checkInputWidth(target, self = true) {
+    const input = self ? this : target;
     const hideInput = input.clientHeight > 40 ? hide : hideSmall;
 
     hideInput.textContent = input.value;
     input.style.width = hideInput.offsetWidth + "px";
-}));
+}
 
+inputArr.forEach(input => {
+    checkInputWidth(input, false);
+
+    input.addEventListener('input', checkInputWidth);
+});
+
+const observerCollection = document.getElementsByClassName('observer');
+Array.from(observerCollection).forEach(item => {
+    const observer = new MutationObserver(() => inputArr.forEach(input => checkInputWidth(input, false)));
+
+    observer.observe(item, { attributes: true });
+});
